4ca64d5829cd09e2042bbf59d602ebb8ced8306b
uchikage <shaundiozh@gmail.com>
2023-08-25 20:01:04 +0800

    Finished Game + Audio

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -7,16 +7,31 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class BlockScript : MonoBehaviour
 {
+    [Header("References")]
     public GameObject block;
     private Rigidbody rb;
-    public float jumpForce;
     private InterfaceManager ui;
+
+    [Header("Variables")]
+    public float jumpForce;
     private int score = 0;
     public bool isAlive = true;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip point;
+    public AudioClip death;
+    public AudioClip jump;
+
+    //Boolean so that death effects only play once.
+    private bool deathOnce = false;
    
     private void Start()
     {
+        //Get reference for rigid body
         rb = block.GetComponent<Rigidbody>();
+
+        //Get reference to the active interface manager (singleton reference)
         ui = InterfaceManager.instance;
     }
 
@@ -25,32 +40,57 @@ public class BlockScript : MonoBehaviour
         
     }
 
+    //Check anything has entered our object's trigger
     private void OnTriggerEnter(Collider other)
     {
+        //check if trigger has tag "Score"
         if (other.CompareTag("Score"))
         {
+            //Add score
             score++;
+            //Update the UI
             ui.score.text = score.ToString();
-            
+            //Play Audio
+            audioSource.PlayOneShot(point);
         }
     }
 
+    //Check if anything has collided with our object
     private void OnCollisionEnter(Collision collision)
     {
+        //check if collided gameobject has tag "Death"
         if (collision.gameObject.CompareTag("Death"))
         {
+            //Block Death
             isAlive = false;
-            rb.AddForce(-10, 2, 0, ForceMode.Impulse);
+            //Check if death effects has already played once
+            if (!deathOnce)
+            {
+                //Death Effects
+                //Launch the block backwards
+                rb.AddForce(-10, 2, 3, ForceMode.Impulse);
+                //Plays death audio
+                audioSource.PlayOneShot(death);
+                //Changes deathOnce to true so that this code will not run again if block hits another pipe while falling.
+                deathOnce = true;
+            }
+            
         }
         
     }
 
+    //On jump using unity input
     void OnJump()
     {
+        //Checks to see if block is alive
         if (isAlive)
         {
+            //Reset velocity so that jump can be consistent 
             rb.velocity = Vector3.zero;
+            //Add jumping force.
             rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            //Play jump sound
+            audioSource.PlayOneShot(jump);
         }
         
 
